191f1ebaebdbb559dca6dcb147a8dfa3ca200051
Vladimir Martinenko <teslabest87@gmail.com>
2023-01-06 19:30:18 +0100

    refactor

--- server/constants.js
+++ server/constants.js
@@ -5,5 +5,20 @@ module.exports = {
   modelsPath: path.resolve(__dirname, 'db', 'models'),
   seedersPath: path.resolve(__dirname, 'db', 'seeders'),
   migrationsPath: path.resolve(__dirname, 'db', 'migrations'),
-  imagePath: path.resolve(__dirname, '..','public', 'images')
+  imagePath: path.resolve(__dirname, 'public', 'images'),
+  JWT_ACCESS_SECRET : 'secret_key_12343532DFSL',
+  JWT_ACCESS_EXPIRATION_TIME: '30s',
+  JWT_REFRESH_SECRET: 'jhhhhhhhhhgggggg',
+  JWT_REFRESH_EXPIRATION_TIME: '14d',
+  USER: 'user',
+  ADMIN: 'admin'
 };
+// const CONSTANTS = {
+//   JWT_ACCESS_SECRET : 'secret_key_12343532DFSL',
+//   JWT_ACCESS_EXPIRATION_TIME: '30s',
+//   JWT_REFRESH_SECRET: 'jhhhhhhhhhgggggg',
+//   JWT_REFRESH_EXPIRATION_TIME: '14d',
+//   USER: 'user',
+//   ADMIN: 'admin'
+// }
+// module.exports = CONSTANTS
