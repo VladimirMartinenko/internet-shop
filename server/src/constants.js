7a91057e5021dd1e8424c85ed593745cc0e5b94f
Vladimir Martinenko <teslabest87@gmail.com>
2023-04-17 19:14:58 +0200

    add src folder

--- server/src/constants.js
+++ server/src/constants.js
@@ -5,7 +5,7 @@ module.exports = {
   modelsPath: path.resolve(__dirname, 'db', 'models'),
   seedersPath: path.resolve(__dirname, 'db', 'seeders'),
   migrationsPath: path.resolve(__dirname, 'db', 'migrations'),
-  imagePath: path.resolve(__dirname, 'public', 'images'),
+  imagePath: path.resolve(__dirname, '..','public', 'images'),
   JWT_ACCESS_SECRET : 'secret_key_12343532DFSL',
   JWT_ACCESS_EXPIRATION_TIME: '5d',
   JWT_REFRESH_SECRET: 'jhhhhhhhhhgggggg',
