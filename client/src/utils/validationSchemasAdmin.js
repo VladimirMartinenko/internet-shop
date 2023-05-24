1c03d56c4685e7c90cb90766f7c282f6645b4f2e
Vladimir Martinenko <teslabest87@gmail.com>
2023-05-24 17:02:40 +0200

    add validation

---
+++ client/src/utils/validationSchemasAdmin.js
@@ -0,0 +1,19 @@
+import * as yup from "yup";
+
+export const CATEGORY_CREATE_CHEMA = yup.object({
+  name: yup.string("повинно бути строчкою").required("повинно бути заповненим"),
+  sectionId: yup
+    .string("повинно бути строчкою")
+    .required("повинно бути заповненим"),
+});
+export const CATEGORY_UPDATE_CHEMA = yup.object({
+  name: yup.string("повинно бути строчкою").required("повинно бути заповненим"),
+  categoryId: yup
+    .string("повинно бути строчкою")
+    .required("повинно бути заповненим"),
+});
+export const CATEGORY_DELETE_CHEMA = yup.object({
+  categoryId: yup
+    .string("повинно бути строчкою")
+    .required("повинно бути заповненим"),
+});
