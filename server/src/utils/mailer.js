cd62ee637c32cc24b7b92ac1d973771987118f93
Vladimir Martinenko <teslabest87@gmail.com>
2023-08-17 19:07:35 +0200

    mailer

--- server/src/utils/mailer.js
+++ server/src/utils/mailer.js
@@ -7,7 +7,7 @@ const smtpTransport = nodemailer.createTransport({
   secure: true,
   auth:{
     user: 'medimplant2023@ukr.net',
-    pass: 'xsjOtTT2iv840UQy'
+    pass: ''
   },
 },{
   from: 'medimplant <medimplant2023@ukr.net>'
