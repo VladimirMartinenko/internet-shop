90dd0f2fed02a5043525a8f4d6c1dd5bac246148
Vladimir Martinenko <teslabest87@gmail.com>
2024-12-26 20:26:02 +0100

    xax

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
-const http = require('http');
+// const http = require('http');
 
 const app = require('./app');
-const server = http.createServer(app);
+// const server = http.createServer(app);
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
 });
