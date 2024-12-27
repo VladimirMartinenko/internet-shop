e7b9547ab470bac202e2f2b5f117bebfe1ac074b
Vladimir Martinenko <teslabest87@gmail.com>
2024-12-27 18:22:26 +0100

    cvdcd

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,5 +1,5 @@
 const CONSTANTS = {
-  HTTP_SERVER_URL: 'http://684713-vds-teslabest87.gmhost.pp.ua:5000/api/',
+  HTTP_SERVER_URL: 'http://684713-vds-teslabest87.gmhost.pp.ua/api/',
   REFRESH_TOKEN: "REFRESH_TOKEN",
   HTTP_SERVER_URL_images: 'http://684713-vds-teslabest87.gmhost.pp.ua:80/images/',
   PRODUCT_IMAGE_PATH: '/staticImages/product.png',
