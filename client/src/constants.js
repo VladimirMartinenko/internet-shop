746954dcee158f686d9a1b61eb67ffa8309fb4a1
Vladimir Martinenko <teslabest87@gmail.com>
2024-12-27 11:57:35 +0100

    casc

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,7 +1,7 @@
 const CONSTANTS = {
-  HTTP_SERVER_URL: 'https://684713-vds-teslabest87.gmhost.pp.ua:5000/api/',
+  HTTP_SERVER_URL: 'http://684713-vds-teslabest87.gmhost.pp.ua:5000/api/',
   REFRESH_TOKEN: "REFRESH_TOKEN",
-  HTTP_SERVER_URL_images: 'https://684713-vds-teslabest87.gmhost.pp.ua:80/images/',
+  HTTP_SERVER_URL_images: 'http://684713-vds-teslabest87.gmhost.pp.ua:80/images/',
   PRODUCT_IMAGE_PATH: '/staticImages/product.png',
 };
 
